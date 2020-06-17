a3273ee1a0697b0c065354df5613b092babe2275
Subash Chaudhary <subashchaudhary@pop-os.localdomain>
2020-06-17 19:53:47 +0545

    text styling

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,24 @@ void main() => runApp(MaterialApp(
     appBar: AppBar(
       title: Text('first flutter app'),
       centerTitle: true,
+      backgroundColor: Colors.red[600],
     ),
     body: Center(
-      child: Text('Hello Nexians'),
+      child: Text(
+        'Hello Nexians',
+        style: TextStyle(
+          fontFamily: 'Roboto',
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2.0,
+          color: Colors.grey[600],
+        ),
+      ),
     ),
     floatingActionButton: FloatingActionButton(
+      onPressed: () {},
       child: Text('click'),
+      backgroundColor: Colors.red[600],
     ),
   )
 
