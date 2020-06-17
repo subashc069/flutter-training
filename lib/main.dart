20cba4c650f99d1198b50f320e0a4e1f43643631
Subash Chaudhary <subashchaudhary@pop-os.localdomain>
2020-06-17 20:16:20 +0545

    build method and hot reload

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,35 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('first flutter app'),
-      centerTitle: true,
-      backgroundColor: Colors.red[600],
-    ),
-    body: Center(
-      child: Text(
-        'Hello Nexians',
-        style: TextStyle(
-          fontFamily: 'Roboto',
-          fontSize: 20.0,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 2.0,
-          color: Colors.grey[600],
-        ),
-      ),
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: () {},
-      child: Text('click'),
-      backgroundColor: Colors.red[600],
-    ),
-  )
-
+  home: Home()
 ));
 
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('first flutter app'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+        child: Text(
+          'Hello Nexians',
+          style: TextStyle(
+            fontFamily: 'Roboto',
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey[600],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('click'),
+        backgroundColor: Colors.red[600],
+      ),
+    );
+  }
+}
