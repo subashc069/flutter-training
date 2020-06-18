3dae3d7d2a361b3481641fb6357b216a7dd44276
Subash Chaudhary <bcoolboy8@gmail.com>
2020-06-18 17:21:45 +0545

    Custom class

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'quote.dart';
 
 void main() => runApp(MaterialApp(
   home: QuoteList()
@@ -12,10 +13,10 @@ class QuoteList extends StatefulWidget {
 
 class _QuoteListState extends State<QuoteList> {
 
-  List<String> quotes = [
-    'As you sow, so you reap',
-    'Tit for tat',
-    'Be yourself because everybody else is taken'
+  List<Quote> quotes = [
+    Quote(author: 'subash', text: 'As you sow, so you reap'),
+    Quote(author: 'manish', text: 'Tit for tat'),
+    Quote(author: 'rajnish', text: 'Be yourself because everybody else is taken'),
   ];
   @override
   Widget build(BuildContext context) {
@@ -28,7 +29,7 @@ class _QuoteListState extends State<QuoteList> {
         elevation: 0,
       ),
       body: Column(
-        children: quotes.map((q) => Text(q, style: TextStyle(color: Colors.red[300]),)).toList(),
+        children: quotes.map((q) => Text('${q.author} - ${q.text}', style: TextStyle(color: Colors.red[300]),)).toList(),
       ),
     );
   }
