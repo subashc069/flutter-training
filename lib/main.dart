2616de63bbf26194399eb4b103562d4cb519b85d
Subash Chaudhary <bcoolboy8@gmail.com>
2020-06-18 18:31:17 +0545

    World Time App

--- lib/main.dart
+++ lib/main.dart
@@ -1,84 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'quote.dart';
+import 'package:nexus/pages/home.dart';
+
 
 void main() => runApp(MaterialApp(
-  home: QuoteList()
+  home: Home()
 ));
 
-class QuoteList extends StatefulWidget {
-  @override
-  _QuoteListState createState() => _QuoteListState();
-}
-
-class _QuoteListState extends State<QuoteList> {
-
-  List<Quote> quotes = [
-    Quote(author: 'Plato', text: 'As you sow, so you reap'),
-    Quote(author: 'Oscar Wilde', text: 'Tit for tat'),
-    Quote(author: 'Steve Jobs', text: 'Be yourself because everybody else is taken'),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[300],
-      appBar: AppBar(
-        title: Text('Awesome Quotes'),
-        centerTitle: true,
-        backgroundColor: Colors.redAccent,
-        elevation: 0,
-      ),
-      body: Column(
-        children: quotes.map((quote) => QuoteCard(
-          quote: quote,
-          delete: (){
-            setState(() {
-              quotes.remove(quote);
-            });
-          }
-          )).toList(),
-      ),
-    );
-  }
-}
-
-class QuoteCard extends StatelessWidget {
-  final Quote quote;
-  final Function delete;
-  QuoteCard({this.quote, this.delete});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Text(
-              quote.text,
-              style: TextStyle(
-                color: Colors.grey[600],
-              ),
-            ),
-            SizedBox(height: 6.0),
-            Text(
-              quote.author,
-              style: TextStyle(
-                color: Colors.grey[600],
-                fontWeight: FontWeight.bold
-              ),
-            ),
-            SizedBox(height: 10.0),
-            FlatButton.icon(
-              onPressed: delete, 
-              icon: Icon(Icons.delete), 
-              label: Text('Delete'))
-          ],
-        ),
-      ),
-    );
-  }
-}
