a63a2d6a27db487fe3d145cfb4420f2210e26495
Subash Chaudhary <bcoolboy8@gmail.com>
2020-06-18 16:07:57 +0545

    list using map function

--- lib/main.dart
+++ lib/main.dart
@@ -2,105 +2,34 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: NinjaIdCard()
+  home: QuoteList()
 ));
 
-class NinjaIdCard extends StatefulWidget {
+class QuoteList extends StatefulWidget {
   @override
-  _NinjaIdCardState createState() => _NinjaIdCardState();
+  _QuoteListState createState() => _QuoteListState();
 }
 
-class _NinjaIdCardState extends State<NinjaIdCard> {
-  int ninjaLevel = 0;
+class _QuoteListState extends State<QuoteList> {
 
+  List<String> quotes = [
+    'As you sow, so you reap',
+    'Tit for tat',
+    'Be yourself because everybody else is taken'
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[300],
       appBar: AppBar(
-        title: Text('Ninja Id Card'),
+        title: Text('Awesome Quotes'),
         centerTitle: true,
-        backgroundColor: Colors.grey[850],
+        backgroundColor: Colors.redAccent,
+        elevation: 0,
       ),
-      backgroundColor: Colors.grey[900],
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        backgroundColor: Colors.grey[800],
-        onPressed: () {
-          setState(() {
-            ninjaLevel += 1;
-          });
-        }
+      body: Column(
+        children: quotes.map((q) => Text(q, style: TextStyle(color: Colors.red[300]),)).toList(),
       ),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/nature-1.jpg'),
-                radius: 50.0,
-              ),
-            ),
-            Divider(
-              height: 90.0,
-              color: Colors.grey[800]
-            ),
-            Text(
-              'NAME',
-              style: TextStyle(
-                color: Colors.grey,
-                letterSpacing: 2.0
-              ),
-            ),
-            SizedBox(height: 10.0),
-            Text(
-              'CHUN LI',
-              style: TextStyle(
-                color: Colors.amberAccent[400],
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 2.0
-              ),
-            ),
-            SizedBox(height: 30.0),
-            Text(
-              'NINJA LEVEL',
-              style: TextStyle(
-                color: Colors.grey,
-                letterSpacing: 2.0
-              ),
-            ),
-            SizedBox(height: 10.0),
-            Text(
-              '$ninjaLevel',
-              style: TextStyle(
-                color: Colors.amberAccent[400],
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 2.0
-              ),
-            ),
-            SizedBox(height: 30.0),
-            Row(
-              children: <Widget>[
-                Icon(
-                  Icons.email,
-                  color: Colors.grey[400]
-                ),
-                SizedBox(width: 10.0),
-                Text(
-                  'bcoolboy8@gmail.com',
-                  style: TextStyle(
-                    fontSize: 18.0,
-                    color: Colors.grey[500]
-                  ),
-                )
-              ],
-            )
-          ],
-        ),
-        ),
     );
   }
 }
